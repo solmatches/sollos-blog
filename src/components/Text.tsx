42269359e5418d01ed8d451e06028bf0b277b039
Mika <solinepik@gmail.com>
2022-03-09 07:02:27 +0900

    chore: eslint jsx-uses-react rules 추가

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,10 +1,10 @@
+import React from 'react'
 interface TextProps {
-    text: string
-  }
-  
-  const Text = function ({ text }: TextProps) {
-    return <div>{text}</div>
-  }
-  
-  export default Text
-  
+  text: string
+}
+
+const Text = function ({ text }: TextProps) {
+  return <div>{text}</div>
+}
+
+export default Text
