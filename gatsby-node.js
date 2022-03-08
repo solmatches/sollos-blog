06275cfab2d617f03664becbc540ffca58280c8b
Mika <solinepik@gmail.com>
2022-03-09 06:24:38 +0900

    feat: tsconfig 추가 및 src 폴더 내 절대경로 설정

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,20 @@
+const path = require("path")
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
+  })
+}
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
