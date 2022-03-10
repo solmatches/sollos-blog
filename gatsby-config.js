3f4d8ae0c93ee1c7c94150a3ffea16ebdbb4c6c3
Mika <solinepik@gmail.com>
2022-03-10 23:49:29 +0900

    feat: emotion 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,14 +14,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-image`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
+    `gatsby-plugin-emotion`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
   ],
