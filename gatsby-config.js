5fb3d6675544606f38f07249147dcfb9501b136d
Mika <solinepik@gmail.com>
2022-03-22 22:41:29 +0900

    chore: markdown, import sort 관련 패키지 설치
    - gatsby-transformer-remark
      - 마크다운 문법을 HTML 형태로 변환해줌
    
    - gatsby-remark-prismjs & prismjs
      - 소스코드를 실제 IDE에서 보는 것 처럼 변환해줌
    
    - gatsby-remark-smartypants
      문서 내에서 사용되는 문장 부호들을 더 깔끔한 부호로 변경해줌
    
    - gatsby-remark-image
      - 마크다운 문서 내에서 이미지 사용을 최적화 해줌 (반응형 이미지 생성, 동적 로딩 등)
    
    - gatsby-remark-copy-linked-files
      - 마크다운 문서 내에서 사용하는 이미지를 특정 디렉토리로 복사해줌 ( 디폴트는 루트 디렉토리의 `public`)
    
    - gatsby-remark-external-links
      - 마크다운 내의 링크 태그의 target, rel 등 속성을 지정해줌
    
    - eslint-plugin-simple-import-sort
      - import 하는 모듈들을 정렬해줌
      - eslint-plugin-react 종속성으로 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,49 @@ module.exports = {
     `gatsby-plugin-emotion`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `contents`,
+        path: `${__dirname}/contents`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-smartypants',
+            options: {
+              dashes: 'oldschool',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 768,
+              quality: 100,
+              withWebp: true,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+          },
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow',
+            },
+          },
+        ],
+      },
+    },
   ],
 }
